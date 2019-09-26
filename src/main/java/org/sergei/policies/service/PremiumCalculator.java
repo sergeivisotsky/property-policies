b8ae7ce3b2e024ebecd0aa61b3b68f32e9d68ed6
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-09-26 19:57:28 +0300

    Premium calculation added

--- src/main/java/org/sergei/policies/service/PremiumCalculator.java
+++ src/main/java/org/sergei/policies/service/PremiumCalculator.java
@@ -1,10 +1,37 @@
 package org.sergei.policies.service;
 
+import org.sergei.policies.dto.PolicySubObject;
 import org.sergei.policies.dto.Policy;
+
+import java.util.List;
 
 /**
  * @author Sergei Visotsky
  */
 public interface PremiumCalculator {
-    Double calculate(Policy policy);
+
+    /**
+     * Calculate premium
+     *
+     * @param policy with all the sub objects
+     * @return calculated premium value
+     */
+    double calculate(Policy policy);
+
+    /**
+     * Method to calculate premium depending on the coefficients
+     *
+     * @param policySubObjects all policy sub-objects
+     * @return calculated premium fire
+     */
+    double calculatePremiumFire(List<PolicySubObject> policySubObjects);
+
+    /**
+     * Calculate premium water
+     *
+     * @param policySubObjects all policy sub-objects
+     * @return calculated premium water
+     */
+    double calculatePremiumWater(List<PolicySubObject> policySubObjects);
+
 }
--- src/main/java/org/sergei/policies/service/PremiumCalculatorImpl.java
+++ src/main/java/org/sergei/policies/service/PremiumCalculatorImpl.java
@@ -1,8 +1,10 @@
 package org.sergei.policies.service;
 
-import org.sergei.policies.dto.Policy;
 import org.sergei.policies.dto.PolicyObject;
 import org.sergei.policies.dto.PolicySubObject;
+import org.sergei.policies.dto.Policy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +14,20 @@ import java.util.List;
  */
 public class PremiumCalculatorImpl implements PremiumCalculator {
 
-    public Double calculate(Policy policy) {
+    private static final Logger log = LoggerFactory.getLogger(PremiumCalculatorImpl.class);
+
+    private Double coefficientFire = 0.013;
+    private Double coefficientWater = 0.1;
+
+    /**
+     * {@link PremiumCalculator#calculate(Policy)}
+     *
+     * @return calculated premium value
+     */
+    @Override
+    public double calculate(Policy policy) {
+
+
         List<PolicyObject> policyObjects = policy.getPolicyObjects();
         List<PolicySubObject> policySubObjects = new ArrayList<>();
 
@@ -21,6 +36,57 @@ public class PremiumCalculatorImpl implements PremiumCalculator {
             policySubObjects.addAll(subObjects);
         });
 
-        return null;
+        double premiumFire = calculatePremiumFire(policySubObjects);
+        double premiumWater = calculatePremiumWater(policySubObjects);
+
+        return premiumFire + premiumWater;
+    }
+
+    /**
+     * {@link PremiumCalculator#calculatePremiumFire(List)}
+     *
+     * @param policySubObjects all policy sub-objects
+     * @return calculated premium fire
+     */
+    @Override
+    public double calculatePremiumFire(List<PolicySubObject> policySubObjects) {
+        double premiumFire = 0.0;
+        for (PolicySubObject policySubObject : policySubObjects) {
+            Double sumInsured = policySubObject.getSumInsured();
+            if (sumInsured != null) {
+                if (sumInsured > 100) {
+                    coefficientFire = 0.023;
+                }
+                premiumFire = sumInsured * coefficientFire;
+            } else {
+                log.info("Sum insured cannot be null");
+                throw new RuntimeException("Sum insured cannot be null");
+            }
+        }
+        return premiumFire;
+    }
+
+    /**
+     * {@link PremiumCalculator#calculatePremiumWater(List)}
+     *
+     * @param policySubObjects all policy sub-objects
+     * @return calculated premium water
+     */
+    @Override
+    public double calculatePremiumWater(List<PolicySubObject> policySubObjects) {
+        double premiumWater = 0.0;
+        for (PolicySubObject policySubObject : policySubObjects) {
+            Double sumInsured = policySubObject.getSumInsured();
+            if (sumInsured != null) {
+                if (sumInsured >= 10) {
+                    coefficientWater = 0.05;
+                }
+                premiumWater = sumInsured * coefficientWater;
+            } else {
+                log.info("Sum insured cannot be null");
+                throw new RuntimeException("Sum insured cannot be null");
+            }
+        }
+        return premiumWater;
     }
 }
