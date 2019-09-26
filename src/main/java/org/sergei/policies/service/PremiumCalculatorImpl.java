cc821dade9e7efaa1379ae4f6902942bba984c06
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-09-26 18:43:10 +0300

    Method implementation

--- src/main/java/org/sergei/policies/service/PremiumCalculatorImpl.java
+++ src/main/java/org/sergei/policies/service/PremiumCalculatorImpl.java
@@ -1,13 +1,26 @@
 package org.sergei.policies.service;
 
 import org.sergei.policies.dto.Policy;
+import org.sergei.policies.dto.PolicyObject;
+import org.sergei.policies.dto.PolicySubObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Sergei Visotsky
  */
 public class PremiumCalculatorImpl implements PremiumCalculator {
+
     public Double calculate(Policy policy) {
-        // TODO
+        List<PolicyObject> policyObjects = policy.getPolicyObjects();
+        List<PolicySubObject> policySubObjects = new ArrayList<>();
+
+        policyObjects.forEach(policyObject -> {
+            List<PolicySubObject> subObjects = policyObject.getSubObjects();
+            policySubObjects.addAll(subObjects);
+        });
+
         return null;
     }
 }
