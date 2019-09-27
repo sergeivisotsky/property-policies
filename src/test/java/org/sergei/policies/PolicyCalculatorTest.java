ded0f50dd3f1f90998c9e6ed0e5b9acff4338d39
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-09-27 09:41:22 +0300

    Another class + Policy object constructor

--- src/test/java/org/sergei/policies/PolicyCalculatorTest.java
+++ src/test/java/org/sergei/policies/PolicyCalculatorTest.java
@@ -1,14 +1,64 @@
 package org.sergei.policies;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
+import org.sergei.policies.dto.*;
+import org.sergei.policies.service.PremiumCalculator;
+import org.sergei.policies.service.PremiumCalculatorImpl;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Sergei Visotsky
  */
 public class PolicyCalculatorTest {
 
+    private PremiumCalculator premiumCalculator;
+
+    public PolicyCalculatorTest() {
+        premiumCalculator = new PremiumCalculatorImpl();
+    }
+
     @Test
     public void calculateTest() {
-        // TODO
+        Policy policy = PolicyConstructor.constructPolicyStub();
+        double premium = premiumCalculator.calculate(policy);
+        System.out.println(premium);
+    }
+
+    @Test
+    public void calculatePremiumFireTest() {
+        PolicySubObject subObjectOne = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.FIRE)
+                .withSubObjectName("TV")
+                .withSumInsured(120.00)
+                .build();
+        PolicySubObject subObjectTwo = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.FIRE)
+                .withSubObjectName("CAR")
+                .withSumInsured(12009.10)
+                .build();
+        List<PolicySubObject> policySubObjects = ImmutableList.of(subObjectOne, subObjectTwo);
+        double premiumFire = premiumCalculator.calculatePremiumFire(policySubObjects);
+        assertEquals(276.2093, premiumFire, 0);
+    }
+
+    @Test
+    public void calculatePremiumWaterTest() {
+        PolicySubObject subObjectOne = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.WATER)
+                .withSubObjectName("FRIDGE")
+                .withSumInsured(300.00)
+                .build();
+        PolicySubObject subObjectTwo = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.WATER)
+                .withSubObjectName("HOUSE")
+                .withSumInsured(12032.190)
+                .build();
+        List<PolicySubObject> policySubObjects = ImmutableList.of(subObjectOne, subObjectTwo);
+        double premiumWater = premiumCalculator.calculatePremiumWater(policySubObjects);
+        assertEquals(601.6095, premiumWater, 0);
     }
 }
---
+++ src/test/java/org/sergei/policies/PolicyConstructor.java
@@ -0,0 +1,94 @@
+package org.sergei.policies;
+
+import com.google.common.collect.ImmutableList;
+import org.sergei.policies.dto.*;
+
+import java.util.List;
+
+/**
+ * Helper test class to construct complex policy object
+ *
+ * @author Sergei Visotsky
+ */
+class PolicyConstructor {
+
+    /**
+     * So that it was unable to create class instances
+     */
+    private PolicyConstructor() {
+    }
+
+    /**
+     * Construct the whole policy for test purposes only
+     *
+     * @return Constructed {@link Policy}
+     */
+    static Policy constructPolicyStub() {
+        List<PolicyObject> policyObjects = constructPolicyObjects();
+
+        return Policy.newBuilder()
+                .withPolicyNumber("LV19-07-100000-1")
+                .withPolicyStatus(PolicyStatus.APPROVED)
+                .withPremium(1400.22)
+                .withPolicyObjects(policyObjects)
+                .build();
+    }
+
+    /**
+     * Construct policy object of all subObjects
+     *
+     * @return {@link List} of all policy objects {@link PolicyObject}
+     */
+    private static List<PolicyObject> constructPolicyObjects() {
+        List<PolicySubObject> flatPolicySubObjects = constructFlatPolicySubObjects();
+        List<PolicySubObject> carPolicySubObjects = constructCarPolicySubObjects();
+
+        PolicyObject carPolicyObject = PolicyObject.newBuilder()
+                .withObjectName("Car")
+                .withSubObjects(carPolicySubObjects)
+                .build();
+        PolicyObject flatPolicyObject = PolicyObject.newBuilder()
+                .withObjectName("A flat")
+                .withSubObjects(flatPolicySubObjects)
+                .build();
+        return ImmutableList.of(flatPolicyObject, carPolicyObject);
+    }
+
+    /**
+     * Construct flat policy sub objects
+     *
+     * @return @return {@link List} of all policy objects {@link PolicySubObject}
+     */
+    private static List<PolicySubObject> constructFlatPolicySubObjects() {
+        PolicySubObject flatSubObjectOne = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.FIRE)
+                .withSubObjectName("TV")
+                .withSumInsured(120.00)
+                .build();
+        PolicySubObject flatSubObjectTwo = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.WATER)
+                .withSubObjectName("CAR")
+                .withSumInsured(12009.10)
+                .build();
+        return ImmutableList.of(flatSubObjectOne, flatSubObjectTwo);
+    }
+
+    /**
+     * Construct car policy sub objects
+     *
+     * @return @return {@link List} of all policy objects {@link PolicySubObject}
+     */
+    private static List<PolicySubObject> constructCarPolicySubObjects() {
+        PolicySubObject carSubObjectOne = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.WATER)
+                .withSubObjectName("WINDOW")
+                .withSumInsured(1450.00)
+                .build();
+        PolicySubObject carSubObjectTwo = PolicySubObject.newBuilder()
+                .withRiskType(RiskType.FIRE)
+                .withSubObjectName("HOUSE")
+                .withSumInsured(124567.1980)
+                .build();
+        return ImmutableList.of(carSubObjectOne, carSubObjectTwo);
+    }
+}
